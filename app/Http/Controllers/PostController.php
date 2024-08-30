d410c348532d337c7e233b64b4340e0dd21929e1
y0n888 <ayam.k_123@outlook.jp>
2024-08-30 17:58:53 +0900

    クエリビルダkadai

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
     public function index() {
-        return view('post.index');
+        $posts = DB::table('posts')->get();
+        return view('post.index', compact('posts'));
     }
 }
